90303f1a825db48ca44d85f59375824e8531b90a
takahirom <takam.dev@gmail.com>
2014-12-07 23:58:00 +0900

    add each file statement

--- buildSrc/src/main/groovy/ImagesDiff.groovy
+++ buildSrc/src/main/groovy/ImagesDiff.groovy
@@ -1,6 +1,9 @@
 class ImagesDiff{
     def diff(directory1,directory2){
-        new File(directory1)
+        new File(directory1).eachFileRecurse (FileType.FILES) { file ->
+            println file
+        }
+
         new File(directory2)
     }
 }
